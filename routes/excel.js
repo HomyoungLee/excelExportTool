5578928d4bd10794476efdacfef5206849ef4dab
lihongyang3 <lihongyang3@jd.com>
2019-07-22 17:57:09 +0800

    add

---
+++ routes/excel.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+  console.log(req)
+});
+
+module.exports = router;
